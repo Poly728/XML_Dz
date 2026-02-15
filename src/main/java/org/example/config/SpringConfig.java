b75f25144555f8b48d86e324bb7919bf8a664a55
Pridrochen <amdmemg@gmail.com>
2026-02-15 17:34:52 +0300

    Fix component scan and remove bean definitions

--- src/main/java/org/example/config/SpringConfig.java
+++ src/main/java/org/example/config/SpringConfig.java
@@ -9,28 +9,13 @@ import org.springframework.stereotype.Component;
 
 @Configuration
 @PropertySource("classpath:application.properties")
-@ComponentScan(basePackages = "java",
+@ComponentScan(basePackages = "org.example",
         useDefaultFilters = false,
         includeFilters = {
                 @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Component.class),
                 @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = TaskRepository.class),
-                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org\\.example\\..+Repository")
         })
 
 public class SpringConfig {
-    @Bean("taskController")
-    public TaskController taskController(TaskService taskService) {
-        return new TaskController(taskService);
-    }
-
-    @Bean("taskService")
-    public TaskService taskService(TaskRepository taskRepository) {
-        return new TaskService(taskRepository);
-    }
-
-    @Bean("taskRepository")
-    public TaskRepository taskRepository() {
-        return new TaskRepositoryJSON();
-    }
-
 }
